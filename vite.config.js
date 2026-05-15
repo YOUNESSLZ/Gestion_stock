af2799c5de0971ecc8c518b63735aa47930677aa
YOUNESSLZ <youness.lazrak05@gmail.com>
2026-05-15 12:25:34 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,16 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.jsx',
+            input: ['resources/css/app.css', 'resources/js/app.jsx'],
             refresh: true,
         }),
         react(),
     ],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `@import "bootstrap/scss/bootstrap";`
+            }
+        }
+    }
 });
